3ae89aa19d8e93cb193274b9edf11848838cd655
Archana Nk <archanank308@gmail.com>
2024-05-07 23:14:29 +0530

    final gemini code

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,100 +1,106 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart'
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 class HomePage extends StatefulWidget {
-    const HomePage({super,key});
+  const HomePage({super.key});
 
-    @override
-    State<HomePage> createState() => _HomePageState();
-
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
+  final Gemini gemini = Gemini.instance;
 
-    final Gemini gemini= Gemini.instance;
+  List<ChatMessage> messages = [];
 
-    List<ChatMessage> messages= [];
-    ChatUser currentUser= ChatUser(id:"0", firstName:"User");
-    ChatUser geminiUser= ChatUser(id:"1", firstName:"gemini", profileImage:"LOGO.png");
-    @override
-    Widget build(BuildContext context){
-        return Scaffold(
-            appBar: AppBar(
-                centerTitle: true,
-                title: const Text(
-                    "Complaint Box"
+  ChatUser currentUser = ChatUser(id: "0", firstName: "User");
+  ChatUser geminiUser = ChatUser(
+    id: "1",
+    firstName: " complaint chat",
+    profileImage:
+        "https://seeklogo.com/images/G/google-gemini-logo-A5787B2669-seeklogo.com.png",
+  );
 
-                ),
-            ),
-            body: _buildUI(),
+  ChatMessage? get chatMessage => null;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Grievance Chat",
+        ),
+      ),
+      body: _buildUI(),
+    );
+  }
 
-        );
-    }
-    widget _buildUI() {
-        return DashChat(
-            inputOptions: InputOptions(trailing: [ 
-                IconButton(
-                    onPressed: () {},
-                    icon:  const Icon(
-                        Icons.image,
-                        ),
-                )
-            ])
-            
-            
-            currentUser: currentUser, onSend: onSendMessage, messages: messages)
-    }
-    void  _sendMessage(ChatMessage ChatMessage) {
+  Widget _buildUI() {
+    return DashChat(
+      inputOptions: InputOptions(trailing: []),
+      currentUser: currentUser,
+      onSend: _sendMessage,
+      messages: messages,
+    );
+  }
 
-        setState(() {
-            messages= [chatMessage,...messages];
-        });
-        try{
-            String question = chatMessage.text;
-            List<Uint8List>? images;
-            if(chatMessage.medias?.isNotEmpty ?? false ){
-                images=[File(chatMessage.medias!.first.url).readAsBytesSync(),
-                ];
-            }
-            gemini.streamGenerateContent(question,images: images,
-            )
-            .listen (event) {});
-            ChatMessage? lastMessage = messages.firstOrNull;
-            if(lastMessage!=null && lastMessage.user==geminiUser){
-                lastMessage=messages.removeAt(0);
-                String response= .content?.parts.fold("",(previous,current), => "$previous ${current.text}")??"";
-                lastMessage.text+=response;
-                setState( () {
-                    messages = [lastMessage!, ...messages];
-                });
-
-            }else{
-
-                String response= .content?.parts.fold("",(previous,current), => "$previous ${current.text}")??"";
-                ChatMessage message= ChatMessage(user: geminiUser, createdAt: DateTime.now(), text:response,);
-                setState( () {
-                    messages= [message, ...messages];
-
-                }
-
-                )            }
-
-        }catch (e){
-            print(e);
+  void _sendMessage(ChatMessage chatMessage) {
+    setState(() {
+      messages = [chatMessage, ...messages];
+    });
+    try {
+      List<String> department = [
+        "Fireforce",
+        "Panchayath",
+        "Income taxoffice",
+        "Electricity Board"
+      ];
+      String question =
+          'The user is giving the problem / instruction faced by him ; given the list of departments available $department select the most appropriate department to handle this issue and return the name of the department only.\n\n  the complaint is ${chatMessage.text}';
+      List<Uint8List>? images;
+      if (chatMessage.medias?.isNotEmpty ?? false) {
+        images = [
+          File(chatMessage.medias!.first.url).readAsBytesSync(),
+        ];
+      }
+      gemini
+          .streamGenerateContent(
+        question,
+        images: images,
+      )
+          .listen((event) {
+        ChatMessage? lastMessage = messages.firstOrNull;
+        if (lastMessage != null && lastMessage.user == geminiUser) {
+          lastMessage = messages.removeAt(0);
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          lastMessage.text += response;
+          setState(
+            () {
+              messages = [lastMessage!, ...messages];
+            },
+          );
+        } else {
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          ChatMessage message = ChatMessage(
+            user: geminiUser,
+            createdAt: DateTime.now(),
+            text: response,
+          );
+          setState(() {
+            messages = [message, ...messages];
+          });
         }
+      });
+    } catch (e) {
+      print(e);
     }
-    void _sendMediaMessage()  async{
-        ImagePicker picker = ImagePicker();
-        xFile? file = await picker.pickImage(
-            source:ImageSource.gallery,
-            );
-            if(file!=null){
-                ChatMessage chatMessage= ChatMessage (user: currentUser, createdAt: DateTime.now(), text:"Evidence attached", medias:[
-                    ChatMedia(url:file.path, fileName: "",type: MediaType.image,
-                    )
-                ],
-                );
-                _sendMessage(chatMessage);
-            }
-    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 // ignore_for_file: unused_import
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:right_app/SignUpScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:right_app/const.dart';
 import 'package:right_app/firebase_options.dart';
+import 'package:right_app/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  Gemini.init(
+    apiKey: GEMINI_API_KEY,
+  );
   runApp(MyApp());
 }
 
@@ -24,67 +29,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'lib/assets/LOGO.png',
-            height: 150,
-          ),
-          SizedBox(height: 20),
-          Text(
-            'The Right.',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Welcome to The Right.',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(height: 20, width: 20),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              'Empower Your Voice: Report Malpractice Directly to Superiors',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpScreen()),
-              );
-            },
-            child: Text(
-              'GET STARTED',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.black,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Image.asset(
+//             'lib/assets/LOGO.png',
+//             height: 150,
+//           ),
+//           SizedBox(height: 20),
+//           Text(
+//             'The Right.',
+//             style: TextStyle(
+//               fontSize: 24,
+//               fontWeight: FontWeight.bold,
+//               color: Colors.black,
+//             ),
+//           ),
+//           SizedBox(height: 20),
+//           Text(
+//             'Welcome to The Right.',
+//             style: TextStyle(
+//               fontSize: 18,
+//               color: Colors.black,
+//             ),
+//           ),
+//           SizedBox(height: 20, width: 20),
+//           Container(
+//             alignment: Alignment.center,
+//             child: Text(
+//               'Empower Your Voice: Report Malpractice Directly to Superiors',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontSize: 16,
+//                 color: Colors.black,
+//               ),
+//             ),
+//           ),
+//           SizedBox(height: 30),
+//           ElevatedButton(
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => SignUpScreen()),
+//               );
+//             },
+//             child: Text(
+//               'GET STARTED',
+//               style: TextStyle(
+//                 color: Colors.white,
+//               ),
+//             ),
+//             style: ElevatedButton.styleFrom(
+//               backgroundColor: Colors.black,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
